57659e7ec220e40fc79ad860c16f1bfeb8db4d7d
zlatan <zlatan.pham@gmail.com>
2018-11-05 11:53:39 +0700

    project init

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>this is a test</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = mode => ({
+  devServer: {
+    contentBase: "./dist",
+    port: 8888
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.?scss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: { plugins: () => [require("autoprefixer")] }
+          },
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "CSS Grid experiment",
+      template: "src/index.html"
+    })
+  ]
+});
